090df5f0d73d6585b47abff0ef041f17929f9395
Moisés Andrés Alejandro Quispe Herrera <mquispeh@everis.com>
2021-02-27 19:55:08 -0500

    Added portal bg

--- principal.js
+++ principal.js
@@ -5,6 +5,7 @@ window.PhaserGlobal = {
 var juego = new Phaser.Game(400, 540, Phaser.CANVAS, 'bloque_juego');
 
 //Agregando los estados del juego
+juego.state.add('Portal', Portal);
 juego.state.add('Juego', Juego);
 juego.state.add('Juego2', Juego2);
 juego.state.add('Juego3', Juego3);
@@ -13,4 +14,4 @@ juego.state.add('Ganado', Ganado);
 juego.state.add('Terminado', Terminado);
 
 //Inicializamos juego en el estado Juego
-juego.state.start('Juego');
+juego.state.start('Portal');
